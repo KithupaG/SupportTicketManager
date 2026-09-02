cd2c3b2989d47d7ca387fca46ddf898cd2ba3f69
Kithupa G <gkithupa@gmail.com>
2026-09-02 05:48:36 +0530

    Add first controller class

--- src/main/java/com/spring/ems/controller/TicketController.java
+++ src/main/java/com/spring/ems/controller/TicketController.java
@@ -1,4 +1,44 @@
 package com.spring.ems.controller;
 
+import com.spring.ems.dto.AssignTicketRequest;
+import com.spring.ems.dto.TicketRequest;
+import com.spring.ems.dto.TicketResponse;
+import com.spring.ems.service.TicketService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tickets")
 public class TicketController {
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TicketService> createTicket(@RequestBody TicketRequest request) {
+        Long customerId = 1L;
+        TicketResponse created = ticketService.createTicket(customerId, request);
+        return ResponseEntity.ok(created);
+    }
+
+    @PostMapping("/{ticketId}/assign")
+    public ResponseEntity<TicketService> assignTicketToAgent(@PathVariable Long ticketId, @RequestBody AssignTicketRequest request) {
+        TicketResponse assigned = ticketService.assignTicketToAgent(ticketId, request.agentId());
+        return ResponseEntity.ok(assigned);
+    }
+
+    @PostMapping("/{ticketId}/status")
+    public ResponseEntity<TicketService> updateTicketStatus(@PathVariable Long ticketId, @RequestBody TicketRequest request) {
+        TicketResponse updated = ticketService.updateTicketStatus(ticketId, request.status());
+        return ResponseEntity.ok(updated);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TicketResponse>> getAllTickets() {
+        return ResponseEntity.ok(ticketService.getAllTickets());
+    }
 }
